615c8ba96d71a4355d9668ba3c8adc348e79d4f8
Luis Eduardo <edulon2015@gmail.com>
2025-05-18 18:37:21 -0300

    conecao com banco

--- emendas/src/main/java/com/emendas/emendas/model/Emenda.java
+++ emendas/src/main/java/com/emendas/emendas/model/Emenda.java
@@ -2,6 +2,7 @@ package com.emendas.emendas.model;
 
 import com.emendas.emendas.enums.Status;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,18 +15,21 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "emenda")
 public class Emenda {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(length = 100, unique = true)
     private String numero;
     private String autor;
     private String descricao;
+    @Positive
     private double valor;
     @Enumerated(EnumType.STRING) // ou ORDINAL, veja observação abaixo
     private Status status;
     private String municipio;
+    @PastOrPresent
     private LocalDate data;
 }
